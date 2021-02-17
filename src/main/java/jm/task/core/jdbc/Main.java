ab181656d2a76cb0ae57935c4ee6729c60a2b05f
kunjut <v.dimnich@gmail.com>
2021-02-17 20:00:46 +0300

    change import

--- src/main/java/jm/task/core/jdbc/Main.java
+++ src/main/java/jm/task/core/jdbc/Main.java
@@ -3,11 +3,6 @@ package jm.task.core.jdbc;
 import jm.task.core.jdbc.model.User;
 import jm.task.core.jdbc.service.UserService;
 import jm.task.core.jdbc.service.UserServiceImpl;
-import jm.task.core.jdbc.util.Util;
-
-import java.sql.Connection;
-import java.sql.SQLException;
-import java.sql.Statement;
 import java.util.List;
 
 public class Main {
@@ -36,6 +31,5 @@ public class Main {
         // Удаление таблицы
         userService.dropUsersTable();
 
-//        System.out.println((connection.isClosed()) ? "true" : "false");
     }
 }
