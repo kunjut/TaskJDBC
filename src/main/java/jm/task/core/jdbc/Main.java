2efeff5c404fcdc1d61c268f7f64c03426e82285
kunjut <v.dimnich@gmail.com>
2021-02-19 22:44:50 +0300

    Add close sessionFactory

--- src/main/java/jm/task/core/jdbc/Main.java
+++ src/main/java/jm/task/core/jdbc/Main.java
@@ -3,6 +3,8 @@ package jm.task.core.jdbc;
 import jm.task.core.jdbc.model.User;
 import jm.task.core.jdbc.service.UserService;
 import jm.task.core.jdbc.service.UserServiceImpl;
+import jm.task.core.jdbc.util.Util;
+
 import java.util.List;
 
 public class Main {
@@ -31,5 +33,7 @@ public class Main {
         // Удаление таблицы
         userService.dropUsersTable();
 
+        // Одно закрытие sessionFactory на все методы
+        Util.getSessionFactory().close();
     }
 }
